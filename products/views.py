5b59265cb67b7d0a3e9b60266b87f91d8064a20b
masoud <masoudtajik1300@gmail.com>
2024-01-20 10:41:57 -0800

    complete product.html and detail.html and show products

--- products/views.py
+++ products/views.py
@@ -1,39 +1,43 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, RedirectView, ListView, DetailView
-from .models import Product,Image
+from .models import Product, Image, Category
 from order.models import OrderItem
+
 
 # Create your views here.
 
 
-
 class Products(ListView):
-    template_name='products/product.html'
-    model=Product #object Product.objects.filter(id=pk)
-    context_object_name='products_all'
+    template_name = 'products/product.html'
+    model = Product  # object Product.objects.filter(id=pk)
+    context_object_name = 'products_all'
+    paginate_by = 6
+
     def get_queryset(self):
         return Product.objects.filter(category__id=self.kwargs['pk'])
-    
-    def get_context_data(self, **kwargs) :
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["products"] = Product.objects.ten_product_new()
-        context["top_cells"] =OrderItem.top_cell_product()
-        context["ten_discounts"] =Product.objects.ten_is_discount()
+        context["top_cells"] = OrderItem.top_cell_product()
+        context["ten_discounts"] = Product.objects.ten_is_discount()
         return context
-    
-   
+
+
 class DetailProduct(ListView):
-    template_name='products/detail.html'
-    model=Product #object Car.objects.filter(id=pk)
-    slug_field='slug'
-    context_object_name='product'
+    template_name = 'products/details.html'
+    model = Product  # object Car.objects.filter(id=pk)
+    slug_field = 'slug'
+    context_object_name = 'product'
+
     # queryset=Product.objects.get(slug=slug_field)
     def get_queryset(self):
         return Product.objects.get(slug=self.kwargs['slug'])
-        
-    def get_context_data(self, **kwargs) :
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["images"] = Image.objects.filter(product=self.get_queryset())
+
+        context["relateds"] = Product.objects.filter(category=self.get_queryset().category).exclude(
+            id=self.get_queryset().id)
         return context
-    
-    
