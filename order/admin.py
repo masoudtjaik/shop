c594d7beaa18197e09083bdae706b36ad4f544a7
masoud <masoudtajik1300@gmail.com>
2024-01-25 01:05:12 -0800

    edit panel admin

--- order/admin.py
+++ order/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Order,OrderItem
+from .models import Order, OrderItem
+
+
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
-    model=OrderItem
+    model = OrderItem
     raw_id_fields = ('product',)
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'update_at', 'is_paid')
-    readonly_fields=('total', )
+    list_display = ('user', 'update_at', 'is_paid')
+    readonly_fields = ('total',)
     list_filter = ('is_paid',)
-    inlines = (OrderItemInline,) 
+    inlines = (OrderItemInline,)
+
+
--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Product,Discount,Like,Comment,Category,Image
+from .models import Product, Discount, Like, Comment, Category, Image
+
 # Register your models here.
 
-admin.site.register((Product,Discount,Like,Comment,Category,Image))
+admin.site.register((Discount,  Category, Image))
+
+
+@admin.register(Product)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'inventory', 'price')
+    search_fields = ('name',)
+    readonly_fields = ('price_discount',)
+
+
+@admin.register(Like)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product')
+
+
+@admin.register(Comment)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product')
